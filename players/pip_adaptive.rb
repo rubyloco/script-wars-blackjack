7843a6a3f6082d7acb14c09ec491bdbdb53efa72
Benjamin Ward <ward.programm3r@gmail.com>
2015-08-11 13:06:10 -0400

    Framework for storage/loading of YAML hand data

--- players/pip_adaptive.rb
+++ players/pip_adaptive.rb
@@ -1,10 +1,31 @@
+require "yaml"
 require 'pip_ai/node'
+
 module ScriptWarsBlackjack
   module Players
     # Pip - By user
 
     class Pip
       include PipAI
+
+      def initialize()
+        @storage_path = File.expand_path("../pip_adaptive_storage.yml", __FILE__)
+        @data = {}
+        save_data(@storage_path)
+        File.new(@storage_path) unless File.exists?(@storage_path)
+        load_data(@storage_path)
+      end
+
+      def load_data(yml_file)
+        @data = YAML.load(File.open(yml_file))
+      end
+
+      def save_data(yml_file)
+        File.open(yml_file, "w") do |file|
+          file.write(@data.to_yaml)
+        end
+      end
+
       def place_bet(game_state)
         # Return your integer bet
         0
